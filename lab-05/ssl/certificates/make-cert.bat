dc4001b9f394112bbc4f500721d55e3364652506
ChiVan <vanchiphung2k5@GMAIL.COM>
2026-06-19 14:07:04 +0700

    SSl

---
+++ lab-05/ssl/certificates/make-cert.bat
@@ -0,0 +1,3 @@
+@echo off
+set "PATH=%PATH%;C:\Program Files\OpenSSL-Win64\bin"
+openssl req -new -x509 -newkey rsa:2048 -nodes -keyout server-key.key -out server-cert.crt -days 365 -config server-cert.cnf
